b1793e1036784b9969ff8e7f5342b55f37412c4f
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2020-06-15 20:09:43 -0700

    Add Best and New stories fetch fns (#2)

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod hn_client;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Fetch top 20 + get data
-    let results = hn_client::get_top_stories(0, 20).await?;
+    let results = hn_client::get_stories(hn_client::StoryListType::Top, 0, 20).await?;
     println!("{:?}", results);
     Ok(())
 }
